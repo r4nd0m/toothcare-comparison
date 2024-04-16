a396f0f71f32c2758e4631a80cfa4892a5aa8795
Vladimir Kalenyuk <vladimir.kalenyuk@adesso.de>
2024-04-16 10:07:27 +0200

    replace deprecated async function with recommended waitForAsync

--- src/app/mainview/insurance-provider/insurance-provider.component.spec.ts
+++ src/app/mainview/insurance-provider/insurance-provider.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { waitForAsync, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { InsuranceProviderComponent } from './insurance-provider.component';
 import { AppModule } from '../../app.module';
@@ -7,7 +7,7 @@ describe('InsuranceProviderComponent', () => {
   let component: InsuranceProviderComponent;
   let fixture: ComponentFixture<InsuranceProviderComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       imports: [ AppModule ],
       declarations: [ InsuranceProviderComponent ]
--- src/app/mainview/mainview.component.spec.ts
+++ src/app/mainview/mainview.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { waitForAsync, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MainviewComponent } from './mainview.component';
 import { AppModule } from '../app.module';
@@ -7,7 +7,7 @@ describe('MainviewComponent', () => {
   let component: MainviewComponent;
   let fixture: ComponentFixture<MainviewComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       imports: [ AppModule ],
       declarations: [ MainviewComponent ]
