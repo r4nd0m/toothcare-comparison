06ef91a156048a76fcea66887cf4f6524b3c0d29
Vladimir Kalenyuk <vladimir.kalenyuk@adesso.de>
2024-04-18 14:01:32 +0200

    extend provider management e2e tests

--- cypress/e2e/provider_management.cy.ts
+++ cypress/e2e/provider_management.cy.ts
@@ -4,9 +4,10 @@ describe('Test provider management', () => {
 
     // there should be 5 default providers on start
     cy.get('insurance-provider').should("have.length", 5);
+    cy.get('.message-no-providers').should("not.exist");
 
     // remove the first provider
-    cy.get('insurance-provider:first-of-type button.button-remove-provider').click();
+    cy.get('insurance-provider:first button.button-remove-provider').click();
 
     // there should be 4 providers after removal
     cy.get('insurance-provider').should("have.length", 4);
@@ -17,5 +18,13 @@ describe('Test provider management', () => {
     // there should be 5 providers after adding
     cy.get('insurance-provider').should("have.length", 5);
 
+    // remove all providers
+    cy.get('insurance-provider button.button-remove-provider').each(($el) => {
+        cy.wrap($el).click();
+    })
+    
+    cy.get('insurance-provider').should("have.length", 0);
+
+    cy.get('.message-no-providers').should("exist");
   })
 })
