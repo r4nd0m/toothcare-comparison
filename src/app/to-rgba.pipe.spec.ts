8bbd9cbb6b5a4f4234585d7b02ec30efafe5e197
Vladimir Kalenyuk <vladimir.kalenyuk@adesso.de>
2024-05-22 16:49:22 +0200

    implement custom pipe to transform color values into rgba format

--- src/app/to-rgba.pipe.spec.ts
+++ src/app/to-rgba.pipe.spec.ts
@@ -1,19 +1,19 @@
 import { ToRgbaPipe } from './to-rgba.pipe';
 
 describe('ToRgbaPipe', () => {
-  it('create an instance', () => {
-    const pipe = new ToRgbaPipe();
-    expect(pipe).toBeTruthy();
-  });
+    it('create an instance', () => {
+        const pipe = new ToRgbaPipe();
+        expect(pipe).toBeTruthy();
+    });
 
-  it('transforms [42, 42, 42] to "rgba(42,42,42,1)"', () => {
-    const pipe = new ToRgbaPipe();
-    expect(pipe.transform([42, 42, 42])).toBe("rgba(42,42,42,1)");
-  });
+    it('transforms [42, 42, 42] to "rgba(42,42,42,1)"', () => {
+        const pipe = new ToRgbaPipe();
+        expect(pipe.transform([42, 42, 42])).toBe("rgba(42,42,42,1)");
+    });
 
-  
-  it('transforms [42, 42, 42], 0.5 to "rgba(42,42,42,1)"', () => {
-    const pipe = new ToRgbaPipe();
-    expect(pipe.transform([42, 42, 42], 0.5)).toBe("rgba(42,42,42,0.5)");
-  });
+
+    it('transforms [42, 42, 300], 0.5 to "rgba(42,42,42,1)"', () => {
+        const pipe = new ToRgbaPipe();
+        expect(pipe.transform([42, 42, 300], 0.5)).toBe("rgba(42,42,255,0.5)");
+    });
 });
--- src/app/to-rgba.pipe.ts
+++ src/app/to-rgba.pipe.ts
@@ -1,14 +1,24 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'toRgba'
+    name: 'toRgba'
 })
 export class ToRgbaPipe implements PipeTransform {
 
-  transform(value: [number, number, number], ...args: [number?]): string {
-    const alpha: number = args[0] || 1;
-    
-    return 'rgba(' + value.join(',') + ',' + alpha + ')';
-  }
+    transform(colors: [number, number, number], ...args: [number?]): string {
+        let normalizedAlpha: number = args[0] || 1;
+
+        if (normalizedAlpha < 0) normalizedAlpha = 0;
+        if (normalizedAlpha > 1) normalizedAlpha = 1;
+
+        return 'rgba(' + colors.map(this.normalizeColor).join(',') + ',' + normalizedAlpha + ')';
+    }
+
+    private normalizeColor(value: number) {
+        if (value < 0) value = 0;
+        if (value > 255) value = 255;
+
+        return value;
+    }
 
 }
