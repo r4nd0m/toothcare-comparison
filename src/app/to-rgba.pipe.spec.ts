9c7b2d8ac76a800f2a270a5f766ddb0378b4f7b1
Vladimir Kalenyuk <vladimir.kalenyuk@adesso.de>
2024-05-22 16:53:06 +0200

    add more tests for the color transformation pipe

--- src/app/to-rgba.pipe.spec.ts
+++ src/app/to-rgba.pipe.spec.ts
@@ -16,4 +16,14 @@ describe('ToRgbaPipe', () => {
         const pipe = new ToRgbaPipe();
         expect(pipe.transform([42, 42, 300], 0.5)).toBe("rgba(42,42,255,0.5)");
     });
+
+    it('transforms [42, 42, 42], 5.5 to "rgba(42,42,42,1)"', () => {
+        const pipe = new ToRgbaPipe();
+        expect(pipe.transform([42, 42, 42], 5.5)).toBe("rgba(42,42,42,1)");
+    });
+
+    it('transforms [42, 42, -42], 0.5 to "rgba(42,42,0,0.5)"', () => {
+        const pipe = new ToRgbaPipe();
+        expect(pipe.transform([42, 42, -42], 0.5)).toBe("rgba(42,42,0,0.5)");
+    });
 });
