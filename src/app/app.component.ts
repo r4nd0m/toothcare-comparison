777a3a3447162bdf39e6e436feee7ad66bbda854
Vladimir Kalenyuk <vladimir.kalenyuk@adesso.de>
2024-05-28 10:45:20 +0200

    remove unused code (routing has been previously moved to the routing module)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,8 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrl: './app.component.css'
 })
-export class AppComponent {
-    title = 'toothcare-comparison';
-    router: Router;
-
-    constructor(router: Router) {
-        this.router = router;
-    }
-}
+export class AppComponent {}
