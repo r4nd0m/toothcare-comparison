bbd4a760e7f1ff3326a2aadf81c49111a6fac6e6
Vladimir Kalenyuk <vladimir.kalenyuk@adesso.de>
2024-06-05 15:18:31 +0200

    restore deleted jest config file used in watch mode

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    preset: 'jest-preset-angular',
+    roots: ['src'],
+    setupFilesAfterEnv: ['./src/setup.jest.ts'],
+    moduleNameMapper: {
+        "^lodash-es$": "lodash"
+    }
+};
