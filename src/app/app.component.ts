5c90ce537ab23c3fcedcbffb854101967406f7fc
Vladimir Kalenyuk <vladimir.kalenyuk@adesso.de>
2024-06-06 10:19:46 +0200

    remove unused code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLinkActive, RouterModule, RouterOutlet } from '@angular/router';
-import { DiagramComponent } from "./diagram/diagram.component";
 
 @Component({
     standalone: true,
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterModule, RouterOutlet, RouterLinkActive, DiagramComponent]
+    imports: [RouterModule, RouterOutlet, RouterLinkActive]
 })
 export class AppComponent {}
